f8df48ebfc83fe6414c327d0238dd58d5c020e66
Matt Quinn <mjq0533@gmail.com>
2022-03-30 18:24:35 -0500

    commit tree 6b245672b600376f7a7a2156db7e665f342a1732

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,28 @@
 # your code here
+
+class MySet
+    def initialize(enumberable = [])
+        @hash = {}
+        enumberable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+end
